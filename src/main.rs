279a1f6b5697ca223194a16845f53492b2d64d38
ChrisPWill <chrispwill@gmail.com>
2015-03-26 18:44:04 +1100

    Added Player structure and velocity commands.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Vec2F {
+    x: f64,
+    y: f64,
+}
+
+struct Player {
+    loc: Vec2F,
+    vel: Vec2F,
+}
+
+trait MobileUnit {
+    fn set_vel_x(&mut self, velX: f64);
+    fn set_vel_y(&mut self, velY: f64);
+}
+
+impl MobileUnit for Player {
+    fn set_vel_x(&mut self, vel_x: f64) {
+        self.vel.x = vel_x;
+    }
+    fn set_vel_y(&mut self, vel_y: f64) {
+        self.vel.y = vel_y;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
