7ca23b4120c4b896ab9621ed42f106355f9030db
ChrisPWill <chrispwill@gmail.com>
2015-03-30 17:28:17 +1100

    Create blank window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
+
+extern crate piston;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate opengl_graphics;
+
 pub mod types;
 pub mod entity;
 
+use std::cell::RefCell;
+use opengl_graphics::{
+    GlGraphics,
+    OpenGL,
+    Texture,
+};
+use sdl2_window::Sdl2Window;
+
 fn main() {
-    println!("Hello, world!");
+    let (win_width, win_height) = (1280, 720);
+    let mut window = Sdl2Window::new(
+        OpenGL::_3_2,
+        piston::window::WindowSettings {
+            title: "Space Game".to_string(),
+            size: [win_width, win_height],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 4,
+        }
+    );
+
+    let window = RefCell::new(window);
+
+    for e in piston::events(&window) {
+    }
 }
