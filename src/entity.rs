15cdfba42f6d244a0b28b19edbf548c3740d74e0
Chris Williams <chrispwill@gmail.com>
2015-04-14 18:08:21 +1000

    Project compiles, added Turret and some documentation

--- src/entity.rs
+++ src/entity.rs
@@ -9,24 +9,45 @@ use sprite::Sprite;
 use opengl_graphics::Texture;
 use uuid::Uuid;
 
+/// A trait that provides a draw function that can be called from anything that
+/// needs to be drawn.
 pub trait Drawable {
+    /// Draws a type to the scene. `render_args` provides the current drawable
+    /// area while `scene` is the scene containing any sprites or textures that
+    /// are the type has loaded and will draw to the renderer.
     fn draw(&self, scene: &mut sprite::Scene<Texture>, render_args: event::RenderArgs);
 }
 
+/// A trait that provides basic functions for updating position on a type.
 pub trait MobileUnit {
+    /// Update the position of the type based on the current velocity.
+    ///
+    /// `time_delta` provides the time elapsed since the last position update,
+    /// allowing position to be updated in a manner independent from framerate.
     fn update_pos(&mut self, time_delta: f64);
 
+    /// Updates the velocity in the x axis.
     fn set_vel_x(&mut self, velX: f64);
+    /// Updates the velocity in the y axis.
     fn set_vel_y(&mut self, velY: f64);
 }
 
+/// The `Player` struct. Handles data and functionality that only the player
+/// should have.
 pub struct Player {
+    /// The location of the `Player` relative to the origin (the centre-bottom)
     loc: vecmath::Vector2<f64>,
+    /// The velocity of the `Player`
     vel: vecmath::Vector2<f64>,
+    /// The sprite ID assigned to the sprite used for the current `Player` ship
     sprite: Uuid,
 }
 
 impl Player {
+    /// Generates a new `Player`.
+    ///
+    /// This loads the player sprite and sets the player position to a reasonable
+    /// default (the origin, centre-bottom of screen)
     pub fn new(scene: &mut sprite::Scene<Texture>) -> Player {
         // load sprite
         let path = Path::new("./assets/playerShip1_red.png");
@@ -44,10 +65,14 @@ impl Player {
         Player{loc: [0.0, 0.0], vel: [0.0, 0.0], sprite: id}
     }
 
+    /// Updates the `Player` based on a provided time delta since the last update.
+    ///
+    /// `time_delta` is in seconds
     pub fn update(&mut self, time_delta: f64) {
         self.update_pos(time_delta);
     }
 
+    /// Directly updates the position of the `Player`
     fn set_position(&mut self, x: f64, y: f64) {
         self.loc = [x, y];
     }
@@ -106,8 +131,8 @@ impl MobileUnit for Projectile {
             vecmath::vec2_scale(self.vel.clone(), time_delta));
     }
 
-    fn set_vel_x(&mut self, _: f64) {
-        panic!("can't set x velocity on Projectile");
+    fn set_vel_x(&mut self, vel_x: f64) {
+        self.vel[0] = vel_x;
     }
 
     fn set_vel_y(&mut self, vel_y: f64) {
@@ -124,5 +149,16 @@ impl Drawable for Projectile {
         let height_mult = render_args.height as f64/720.0;
         sprite.set_position(render_args.width as f64/2.0 + (self.loc[0]*width_mult), 
                             render_args.height as f64 - (self.loc[1]*height_mult));
+    }
+}
+
+struct Turret {
+    loc: vecmath::Vector2<f64>, // relative to ship origin
+    rot: f64, // clockwise from +x axis
+}
+
+impl Turret {
+    fn set_rotation(&mut self, r: f64) {
+        self.rot = r;
     }
 }
